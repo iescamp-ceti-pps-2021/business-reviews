a32b8056df2cc02d463dccc0b18fa596a954b782
Francisco González Cavero <43912606+fgonzalezc@users.noreply.github.com>
2022-04-01 17:07:11 +0200

    Variables de entorno

--- docker/mysql/chinook/02_chinook_tables_mediatype.sql
+++
@@ -1,3 +0,0 @@
-LOCK TABLES `MediaType` WRITE;
-INSERT INTO `MediaType` VALUES (1,'MPEG audio file'),(2,'Protected AAC audio file'),(3,'Protected MPEG-4 video file'),(4,'Purchased AAC audio file'),(5,'AAC audio file');
-UNLOCK TABLES;
